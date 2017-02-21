6929b140d82ea80b6ea744dd9a8475970aa05aad
Thibault Defrance <thibault.g.defrance@gmail.com>
2017-02-21 15:49:12 +0100

    -3e_partie

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
     root 'galleries#index'
 
-  namespace :admin do
+    resources :galleries, only: [:index, :show]
+
+    namespace :admin do
     resources :galleries
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
